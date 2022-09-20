ca4c92f221d5913d3b0479f965d50780544488b1
Skallu <patryk.kaminski0711@gmail.com>
2022-09-20 14:20:30 +0200

    updated standalone version

---
+++ TopDownAiSensor2D/TopDownAiSensor2D.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TopDownAiSensor2D : MonoBehaviour
+{
+    #region SENSOR FLAGS
+    [SerializeField] private bool targetInsideViewOuterRadius, targetSpotted, targetInsideSafeZone, targetInsideAttackRange;
+    public bool TargetInsideViewOuterRadius => targetInsideViewOuterRadius;
+    public bool TargetSpotted => targetSpotted;
+    public bool TargetInsideSafeZone => targetInsideSafeZone;
+    public bool TargetInsideAttackRange => targetInsideAttackRange;
+    #endregion
+
+    #region SENSOR PARAMETERS
+    [SerializeField] private float viewOuterRadius, viewInnerRadius, viewAngle;
+    [SerializeField] private bool useSpecialZones;
+    [SerializeField] private float safeZoneRadius, attackRangeRadius;
+    
+    [SerializeField] private float sensorTick = 0.02f;
+    [SerializeField] private LayerMask obstacleLayerMask;
+    [SerializeField] private GameObject targetObject;
+    public GameObject TargetObject => targetObject;
+    #endregion
+    
+    #region SCENE GUI PARAMETERS
+    // these variables are used by custom editor, so do not delete them even though the IDE says they are never used
+    [SerializeField] private Color mainColor, safeZoneColor, attackRangeColor;
+    [SerializeField] private float thickness = 0.5f;
+    #endregion
+
+    private void Awake()
+    {
+        // set target object here
+        if (targetObject == null)
+            targetObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Start() => StartCoroutine(ScanForTargetRoutine());
+
+    /// <summary>
+    /// Performs all zone checks with some delay for the better performance.
+    /// </summary>
+    private IEnumerator ScanForTargetRoutine()
+    {
+        while (true)
+        {
+            CheckForVisibleTarget();
+
+            if (useSpecialZones) // do not perform "safe zone" and "attack range" checks if they aren't used
+            {
+                if (safeZoneRadius > 0) CheckSafeZone();
+                if (attackRangeRadius > 0) CheckAttackRange();
+            }
+
+            yield return new WaitForSeconds(sensorTick);
+        }
+    }
+    
+    /// <summary>
+    /// Checks for visible targets inside view range and angle.
+    /// </summary>
+    private void CheckForVisibleTarget()
+    {
+        var currentPos = transform.position;
+        var targetPos = targetObject.transform.position;
+        
+        if (Vector2.SqrMagnitude(targetPos - currentPos) <= viewOuterRadius * viewOuterRadius) // when target is inside outer view radius
+        {
+            targetInsideViewOuterRadius = true;
+        
+            var directionToTarget = (targetPos - currentPos).normalized;
+            var distanceToTarget = Vector2.Distance(currentPos, targetPos);
+            
+            if (Vector2.SqrMagnitude(targetPos - currentPos) <= viewInnerRadius * viewInnerRadius) // when target is inside inner view radius
+            {
+                // when raycast doesn't collide with any object from obstacle mask, it means, that target is spotted
+                targetSpotted = !Physics2D.Raycast(currentPos, directionToTarget, distanceToTarget, obstacleLayerMask);
+            }
+            else
+            {
+                // when the target is inside view angle and raycast doesn't collide with any object from obstacle mask, it means, that target is spotted
+                targetSpotted = Vector3.Angle(transform.right, directionToTarget) < viewAngle * 0.5f
+                                && !Physics2D.Raycast(currentPos, directionToTarget, distanceToTarget, obstacleLayerMask);
+            }
+        }
+        else
+        {
+            targetInsideViewOuterRadius = false;
+            targetSpotted = false;
+        }
+    }
+    
+    /// <summary>
+    /// Checks if player is inside specific radius
+    /// </summary>
+    /// <param name="radius"> float value considered as "radius" to check (e.g. "safe zone" or "attack range") </param>
+    /// <param name="obstacleMask"> LayerMask considered as obstacle layer, which is used during circle cast </param>
+    private bool CheckIfTargetIsInsideSpecificRadius(float radius, LayerMask obstacleMask)
+    {
+        var currentPos = transform.position;
+        var targetPos = targetObject.transform.position;
+        
+        if (Vector2.SqrMagnitude(targetPos - currentPos) <= radius * radius) // when target is inside inner view radius
+        {
+            var directionToTarget = (targetPos - currentPos).normalized;
+            var distanceToTarget = Vector3.Distance(currentPos, targetPos);
+        
+            if (!Physics2D.Raycast(currentPos, directionToTarget, distanceToTarget, obstacleMask)) return true;
+        }
+    
+        return false;
+    }
+    
+    /// <summary>
+    /// Checks if player is inside character's "safe zone". Value of "target inside safe zone" flag is the result of check action.
+    /// </summary>
+    private void CheckSafeZone() => targetInsideSafeZone = CheckIfTargetIsInsideSpecificRadius(safeZoneRadius, obstacleLayerMask);
+
+    /// <summary>
+    /// Checks if player is inside character's "attack range". Value of "target inside attack range" flag is the result of check action
+    /// </summary>
+    private void CheckAttackRange() => targetInsideAttackRange = CheckIfTargetIsInsideSpecificRadius(attackRangeRadius, obstacleLayerMask);
+    
+}
---
+++ TopDownAiSensor2D/TopDownAiSensor2DEditor.cs
@@ -0,0 +1,239 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+
+// I recommend to put this script inside "Editor" folder
+[CustomEditor(typeof(TopDownAiSensor2D)), CanEditMultipleObjects]
+public class TopDownAiSensor2DEditor : Editor
+{
+    private TopDownAiSensor2D aiSensor;
+    
+    #region PROPERTIES AND GUI CONTENT
+    private SerializedProperty
+        viewOuterRadius, viewInnerRadius, viewAngle, useSpecialZones, safeZoneRadius, attackRangeRadius, sensorTick, obstacleLayerMask, targetObject,
+        mainColor, safeZoneColor, attackRangeColor, thickness,
+        targetInsideViewOuterRadius, targetSpotted, targetInsideSafeZone, targetInsideAttackRange;
+    
+    private static readonly GUIContent ViewOuterRadiusContent = new GUIContent("Outer view radius", "Circular detection area max border");
+    private static readonly GUIContent ViewInnerRadiusContent = new GUIContent("Inner view radius", "Circular detection area min border");
+    private static readonly GUIContent ViewAngleContent = new GUIContent("View angle", "detection angle (in degrees)");
+    private static readonly GUIContent SafeZoneRadiusContent = new GUIContent("Safe zone radius", "(optional) Safe zone area radius");
+    private static readonly GUIContent AttackRangeRadiusContent = new GUIContent("Attack range radius", "(optional) Attack range area");
+    private static readonly GUIContent SensorTickContent = new GUIContent("Update interval", "aiSensor update tick");
+    #endregion
+
+    #region GROUP WRAPPING RELATED FIELDS
+    private bool showSensorParameters = true; // is "Sensor Parameters" foldout header group unwrapped
+    private bool showSceneGuiParameters = true; // is "Scene GUI Parameters" foldout header group unwrapped
+    private bool showSensorFlags = false; // is "Sensor Flags" foldout header group unwrapped
+    #endregion
+
+    private void OnEnable()
+    {
+        aiSensor = (TopDownAiSensor2D) target;
+
+        viewOuterRadius = serializedObject.FindProperty("viewOuterRadius");
+        viewInnerRadius = serializedObject.FindProperty("viewInnerRadius");
+        viewAngle = serializedObject.FindProperty("viewAngle");
+        useSpecialZones = serializedObject.FindProperty("useSpecialZones");
+        safeZoneRadius = serializedObject.FindProperty("safeZoneRadius");
+        attackRangeRadius = serializedObject.FindProperty("attackRangeRadius");
+        sensorTick = serializedObject.FindProperty("sensorTick");
+        obstacleLayerMask = serializedObject.FindProperty("obstacleLayerMask");
+        targetObject = serializedObject.FindProperty("targetObject");
+        
+        mainColor = serializedObject.FindProperty("mainColor");
+        safeZoneColor = serializedObject.FindProperty("safeZoneColor");
+        attackRangeColor = serializedObject.FindProperty("attackRangeColor");
+        thickness = serializedObject.FindProperty("thickness");
+
+        targetInsideViewOuterRadius = serializedObject.FindProperty("targetInsideViewOuterRadius");
+        targetSpotted = serializedObject.FindProperty("targetSpotted");
+        targetInsideSafeZone = serializedObject.FindProperty("targetInsideSafeZone");
+        targetInsideAttackRange = serializedObject.FindProperty("targetInsideAttackRange");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (aiSensor == null) return;
+        
+        // default "script" object field
+        DefaultScriptField(MonoScript.FromMonoBehaviour((MonoBehaviour)target));
+        
+        serializedObject.Update();
+        EditorGUILayout.BeginVertical();
+        
+        #region SENSOR PARAMETERS GROUP
+        showSensorParameters = EditorGUILayout.BeginFoldoutHeaderGroup(showSensorParameters, "Main sensor parameters");
+        if (showSensorParameters)
+        {
+            PropertySliderField(viewOuterRadius, 0, 20, ViewOuterRadiusContent);
+            PropertySliderField(viewInnerRadius, 0, 10, ViewInnerRadiusContent);
+            PropertySliderField(viewAngle, 0, 360, ViewAngleContent);
+
+            // shows "Special Zones" main parameters
+            EditorGUILayout.PropertyField(useSpecialZones);
+            if (EditorGUILayout.BeginFadeGroup(useSpecialZones.boolValue ? 1 : 0))
+            {
+                PropertySliderField(safeZoneRadius, 0, 10, SafeZoneRadiusContent);
+                PropertySliderField(attackRangeRadius, 0, 10, AttackRangeRadiusContent);
+            }
+            EditorGUILayout.EndFadeGroup();
+            EditorGUILayout.Space();
+
+            PropertySliderField(sensorTick, 0.001f, 1, SensorTickContent);
+            EditorGUILayout.PropertyField(obstacleLayerMask);
+            EditorGUILayout.PropertyField(targetObject);
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        EditorGUILayout.Space();
+        #endregion
+
+        #region SCENE GUI PARAMETERS GROUP
+        showSceneGuiParameters = EditorGUILayout.BeginFoldoutHeaderGroup(showSceneGuiParameters, "Scene GUI Parameters");
+        if (showSceneGuiParameters)
+        {
+             PropertySliderField(thickness, 0.5f, 2, default);
+             EditorGUILayout.PropertyField(mainColor);
+
+             // shows "Special Zones" visual parameters
+            if (EditorGUILayout.BeginFadeGroup(useSpecialZones.boolValue ? 1 : 0))
+            {
+                EditorGUILayout.PropertyField(safeZoneColor);
+                EditorGUILayout.PropertyField(attackRangeColor);
+            }
+            EditorGUILayout.EndFadeGroup();
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        EditorGUILayout.Space();
+        #endregion
+
+        #region SENSOR FLAGS GROUP
+        showSensorFlags = EditorGUILayout.BeginFoldoutHeaderGroup(showSensorFlags, "Sensor Flags");
+        if (showSensorFlags)
+        {
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.PropertyField(targetInsideViewOuterRadius);
+            EditorGUILayout.PropertyField(targetSpotted);
+
+            // shows "Special Zones" debug checks
+            if (EditorGUILayout.BeginFadeGroup(useSpecialZones.boolValue ? 1 : 0))
+            {
+                EditorGUILayout.PropertyField(targetInsideSafeZone);
+                EditorGUILayout.PropertyField(targetInsideAttackRange);
+            }
+            EditorGUILayout.EndFadeGroup();
+            EditorGUI.EndDisabledGroup();
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        #endregion
+
+        // apply modified properties and repaint
+        EditorGUILayout.EndVertical();
+        serializedObject.ApplyModifiedProperties();
+
+        if (GUI.changed)
+            InternalEditorUtility.RepaintAllViews();
+    }
+
+    private void OnSceneGUI()
+    {
+        if (aiSensor == null) return;
+        
+        var pos = aiSensor.transform.position;
+
+        // draws outer and inner view radius
+        DrawZone(pos, 360f, viewOuterRadius.floatValue, thickness.floatValue, mainColor.colorValue);
+        DrawZone(pos, 360f, viewInnerRadius.floatValue, thickness.floatValue, mainColor.colorValue);
+
+        // calculates and draws view angle cone
+        var eulerAngles = aiSensor.transform.eulerAngles;
+        var viewAngleLeft = CalculateDirectionFromAngle((-viewAngle.floatValue + 180) * 0.5f, eulerAngles.z); // left view angle: \|
+        var viewAngleRight = CalculateDirectionFromAngle((viewAngle.floatValue + 180) * 0.5f, eulerAngles.z); // right view angle: |/
+        
+        Handles.color = mainColor.colorValue;
+        Handles.DrawLine(pos, pos + viewAngleLeft * viewOuterRadius.floatValue, thickness.floatValue);
+        Handles.DrawLine(pos, pos + viewAngleRight * viewOuterRadius.floatValue, thickness.floatValue);
+        
+        // draws special zones if used
+        if (useSpecialZones.boolValue is true)
+        {
+            DrawZone(pos, 360f, safeZoneRadius.floatValue, thickness.floatValue, safeZoneColor.colorValue); // draws safe zone radius
+            DrawZone(pos, 360f, attackRangeRadius.floatValue, thickness.floatValue, attackRangeColor.colorValue); // draws attack range radius
+        }
+        
+        // draws line from character to spotted target
+        if (aiSensor.TargetInsideViewOuterRadius)
+        {
+            Handles.color = aiSensor.TargetSpotted ? Color.green : Color.red;
+            Handles.DrawLine(pos, aiSensor.TargetObject.transform.position, thickness.floatValue);
+        }
+    }
+
+    #region GUI (FIELD OF VIEW) RELATED METHODS
+    /// <summary>
+    /// Draws Field of View zone wire arc in certain color
+    /// </summary>
+    /// <param name="center"> center position </param>
+    /// <param name="angle"> angle </param>
+    /// <param name="radius"> radius </param>
+    /// <param name="lineThickness"> handles thickness </param>
+    /// <param name="color"> handles color </param>
+    private static void DrawZone(Vector3 center, float angle, float radius, float lineThickness, Color color)
+    {
+        Handles.color = color;
+        Handles.DrawWireArc(center, Vector3.forward, Vector3.up, angle, radius, lineThickness);
+    }
+
+    /// <summary>
+    /// Calculated direction from angle
+    /// </summary>
+    /// <param name="inputAngle"> provided angle </param>
+    /// <param name="inputEulerAngleZ"> provided euler Z angle </param>
+    private static Vector3 CalculateDirectionFromAngle(float inputAngle, float inputEulerAngleZ)
+    {
+        var newAngle = inputAngle - inputEulerAngleZ;
+        return new Vector3(Mathf.Sin(newAngle * Mathf.Deg2Rad), Mathf.Cos(newAngle * Mathf.Deg2Rad), 0f);
+    }
+    #endregion
+
+    #region INSPECTOR UTILS METHODS
+    /// <summary>
+    /// Default "script" object field
+    /// </summary>
+    /// <param name="target"> target object </param>
+    private static void DefaultScriptField(Object target)
+    {
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.ObjectField("Script", target, target.GetType(), false);
+        EditorGUILayout.Space();
+        EditorGUI.EndDisabledGroup();
+    }
+        
+    /// <summary>
+    /// Creates slider field for serialized property
+    /// </summary>
+    /// <param name="property"> serialized property </param>
+    /// <param name="leftValue"> min value </param>
+    /// <param name="rightValue"> max value </param>
+    /// <param name="label"> label (name, tooltip) </param>
+    private static void PropertySliderField(SerializedProperty property, float leftValue, float rightValue, GUIContent label)
+    {
+        var position = EditorGUILayout.GetControlRect();
+            
+        label = EditorGUI.BeginProperty(position, label, property);
+            
+        EditorGUI.BeginChangeCheck();
+        var newValue = EditorGUI.Slider(position, label, property.floatValue, leftValue, rightValue);
+            
+        if (EditorGUI.EndChangeCheck())
+            property.floatValue = newValue;
+            
+        EditorGUI.EndProperty();
+    }
+    #endregion
+}
+
+#endif
